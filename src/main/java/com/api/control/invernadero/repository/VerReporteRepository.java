0db3fd7b70e3cee6e920ff7037be796ab88f345b
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-04-16 11:42:58 -0600

    update

--- src/main/java/com/api/control/invernadero/repository/VerReporteRepository.java
+++ src/main/java/com/api/control/invernadero/repository/VerReporteRepository.java
@@ -9,6 +9,6 @@ import com.api.control.invernadero.entity.Reporte;
 
 @Repository
 public interface VerReporteRepository extends JpaRepository<Reporte, Long> {
-    @Query("SELECT new com.example.invernadero.dto.VerreporteDTO(r.id_reporte, r.fecha, r.hora, r.observaciones) FROM Reporte r")
+    @Query("SELECT new com.api.control.invernadero.dto.VerReporteDto(r.id, r.fecha, r.hora, r.observaciones) FROM Reporte r")
     List<VerReporteDto> findAllCustom();
 }
