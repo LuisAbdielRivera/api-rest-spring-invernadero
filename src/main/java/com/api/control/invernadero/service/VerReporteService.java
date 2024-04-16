274ad480eab9e3bbda2f9433ecfe8f4490eb95ed
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-04-16 11:19:51 -0600

    update

---
+++ src/main/java/com/api/control/invernadero/service/VerReporteService.java
@@ -0,0 +1,18 @@
+package com.api.control.invernadero.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.api.control.invernadero.dto.VerReporteDto;
+import com.api.control.invernadero.repository.VerReporteRepository;
+
+@Service
+public class VerReporteService {
+
+    @Autowired
+    private VerReporteRepository verReporteRepository;
+
+    public List<VerReporteDto> getAllVerreporte() {
+        return verReporteRepository.findAllCustom();
+    }
+}
