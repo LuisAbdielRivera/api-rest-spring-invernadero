3f2d2ed37163d4cd8334914b1dc7a60543780eae
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-04-16 11:24:03 -0600

    update

--- src/main/java/com/api/control/invernadero/controller/VerReporteController.java
+++ src/main/java/com/api/control/invernadero/controller/VerReporteController.java
@@ -1,19 +1,29 @@
 package com.api.control.invernadero.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.api.control.invernadero.entity.Reporte;
-import com.api.control.invernadero.repository.VerReporteRepository;
+import com.api.control.invernadero.dto.VerReporteDto;
+import com.api.control.invernadero.service.VerReporteService;
 
 @RestController
-@RequestMapping("/api/VerReporte")
+@RequestMapping("api/VerReporte")
 public class VerReporteController {
+
     @Autowired
-    private VerReporteRepository verReporteRepository;
+    private VerReporteService verReporteService;
 
     @GetMapping
-    public List<Reporte> getAllReportes() {
-        return verReporteRepository.obtenerSoloDatos();
+    public ResponseEntity<List<VerReporteDto>> getAllVerreporte() {
+        List<VerReporteDto> VerReporteDtos = verReporteService.getAllVerreporte()
+                .stream()
+                .map(reporte -> new VerReporteDto(reporte.getId(), reporte.getFecha(), reporte.getHora(), reporte.getObservaciones()))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(VerReporteDtos, HttpStatus.OK);
     }
+    
 }
